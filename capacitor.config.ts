6ac5d3b16d121b6d635037a4498dd8d820f53855
joonabil145-hash <joonabil145@gmail.com>
2026-09-02 10:13:42 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.studybuddy.app",
+  appName: "رفيق المذاكرة",
+  webDir: "www",
+  bundledWebRuntime: false
+};
+
+export default config;
